868b12f6645c6f5b9b127d6dfb35403a49605d61
Habibur Rahman <habiburrahman0540@gmail.com>
2021-12-31 22:12:34 +0600

    added meal summary component and style

---
+++ src/Components/Meals/MealSummary.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from "./MealSummary.module.css"
+const MealSummary = () => {
+    return (
+        <section className={classes.summary}>
+            <h2>Delicious food ,delivered to you.</h2>
+            <p>Choose your fevorite food from our board selection of available meals and enjoy a delicious lunch or dinner at home. </p>
+            <p>All our meals are cooked with high quality ingredients , just in time and of course by experienced chefs.</p>
+        </section>
+    )
+}
+
+export default MealSummary
