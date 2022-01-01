5623e187c2ac261636a18aeff8854e369ef54756
Habibur Rahman <habiburrahman0540@gmail.com>
2022-01-01 23:15:23 +0600

    Added MealItemForm component and Input component with style

--- src/Components/Meals/AvailableMeals.jsx
+++ src/Components/Meals/AvailableMeals.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
-
+import classes from "./AvailableMeals.module.css"
+import Card from '../UI/Card'
+import MealItem from './MealItem'
+const DUMMY_MEALS = [
+    {
+        id:"m1",
+        name:"Sushi",
+        description:"Finest fish and veggies.",
+        price:22.99
+    },
+    {
+        id:"m2",
+        name:"Schnitzel",
+        description:"A german speciality.",
+        price:16.5
+    },
+    {
+        id:"m3",
+        name:"Barbecue Burger",
+        description:"American , raw , meaty.",
+        price:12.99
+    },
+]
 const AvailableMeals = () => {
+    const meal_list = DUMMY_MEALS.map(meal=><MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>)
     return (
-        <div>
-            
-        </div>
+        <section className={classes.meals}>
+            <Card>
+            <ul>
+                {meal_list}
+            </ul>
+            </Card>
+        </section>
     )
 }
 
